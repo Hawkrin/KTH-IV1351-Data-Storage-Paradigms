881a990d3241c308bb375dc16ab0af9015ee3e50
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-01-31 19:21:49 +0100

    inserts done

---
+++ src/main/resources/lessons_Insert.sql
@@ -0,0 +1,19 @@
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('57', '320', '864', 'guitar', '2021-07-01 11:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('58', '523', '452', 'guitar', '2021-03-26 20:00:00');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('59', '321', '123', 'piano', '2021-02-16 19:50:00');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('60', '421', '654', 'saxophone', '2021-07-01 15:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('61', '321', '725', 'bass', '2021-07-05 16:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('62', '411', '956', 'singing', '2021-07-01 11:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('63', '462', '251', 'guitar', '2021-08-05 11:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('64', '235', '743', 'bass', '2021-09-03 18:30');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('65', '412', '352', 'piano', '2021-06-02 15:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('66', '456', '952', 'maracas', '2021-09-01 11:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('67', '125', '421', 'bass', '2021-05-02 18:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('68', '654', '952', 'saxophone', '2021-09-01 15:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('69', '325', '456', 'synth', '2021-03-01 21:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('70', '320', '854', 'guitar', '2021-09-01 17:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('71', '450', '456', 'bass', '2021-07-02 16:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('72', '154', '254', 'piano', '2021-05-06 12:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('73', '456', '426', 'synth', '2021-07-02 16:20');
+insert into lesson (lesson_id, student_id, instructor_id, instrument_type, date) values ('74', '693', '986', 'bass', '2021-07-01 11:20');
+
